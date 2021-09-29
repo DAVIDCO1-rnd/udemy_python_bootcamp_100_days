40a222c90771263d644dea7956158e044573e633
David Cohn <DAVIDCO1@rnd-hub.com>
2021-09-30 00:42:49 +0300

    updated

--- lessons/lesson_001/thonny_lesson001.py
+++ lessons/lesson_001/thonny_lesson001.py
@@ -1,5 +1,27 @@
-a=5
-b=10
-temp=a
-a=b
-b=temp
+total_bill_str = '124.54'
+num_of_people_str = '5'
+percentage_str = '12'
+
+print('Welcome to the tip calculator.')
+print('What was the total bill? ')
+total_bill = float(total_bill_str)
+print('How many people to split the bill? ')
+num_of_people = int(num_of_people_str)
+print("what percentage? ")
+percentage = float(percentage_str)
+price = (total_bill * (1+percentage/100)) / num_of_people
+float_str = "{:.4f}".format(price)
+full_str = "each person should pay " + float_str
+print(full_str)
+print("hello"[2])
+
+a = 5
+b = 2
+c = 7
+mystr = f"{a} + {b} = {c}"
+print(mystr)
+
+
+
+
+
